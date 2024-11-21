b377265c2fb1ad0b614de79287522fc6b9477d6a
raileme <00001086471246sp@al.educacao.sp.gov.br>
2024-11-21 17:56:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,60 @@
+// Criando os cartões com perguntas sobre o SPFC
+criaCartao(
+    'História',
+    'Quando o São Paulo Futebol Clube foi fundado?',
+    'O São Paulo Futebol Clube foi fundado em 25 de janeiro de 1930.'
+);
+
+criaCartao(
+    'Estádio',
+    'Qual o nome do estádio do SPFC?',
+    'O nome do estádio do SPFC é Cícero Pompeu de Toledo, conhecido como "Morumbi".'
+);
+
+criaCartao(
+    'Títulos',
+    'Quantas vezes o SPFC ganhou a Copa Libertadores?',
+    'O SPFC ganhou a Copa Libertadores 3 vezes: 1992, 1993 e 2005.'
+);
+
+criaCartao(
+    'Jogadores',
+    'Quem é o maior artilheiro da história do SPFC?',
+    'O maior artilheiro da história do SPFC é o centroavante Luís Fabiano.'
+);
+
+criaCartao(
+    'Mundial',
+    'Quantas vezes o SPFC ganhou o Mundial de Clubes da FIFA?',
+    'O SPFC ganhou o Mundial de Clubes da FIFA 3 vezes: 1992, 1993 e 2005.'
+);
+
+criaCartao(
+    'Ídolos',
+    'Quem é considerado o maior ídolo da história do SPFC?',
+    'O maior ídolo da história do SPFC é Rogério Ceni, que foi goleiro e capitão do time por muitos anos.'
+);
+
+criaCartao(
+    'Rivalidades',
+    'Quais são os principais rivais do SPFC?',
+    'Os principais rivais do SPFC são Corinthians, Palmeiras e Santos.'
+);
+
+criaCartao(
+    'Conquistas',
+    'Quantos títulos brasileiros o SPFC possui?',
+    'O SPFC conquistou 6 títulos do Campeonato Brasileiro: 1977, 1986, 1991, 2006, 2007 e 2008.'
+);
+
+criaCartao(
+    'Estrutura',
+    'O SPFC tem quantos títulos internacionais?',
+    'O SPFC possui 12 títulos internacionais, incluindo 3 Copas Libertadores e 3 Mundiais de Clubes.'
+);
+
+criaCartao(
+    'Torcida',
+    'Qual o nome da torcida organizada do SPFC?',
+    'A principal torcida organizada do SPFC é a "Torcida Independente".'
+);
